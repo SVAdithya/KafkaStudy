5a479cc05531e4d787ec4fb08ab156ddabe959f3
SVAdithya <svadithya93@gmail.com>
2024-07-16 09:35:04 +0530

    Capturing with Headers

--- src/main/java/com/kafka/learn/kafkastudy/listener/RegularKafkaListener.java
+++ src/main/java/com/kafka/learn/kafkastudy/listener/RegularKafkaListener.java
@@ -1,5 +1,6 @@
 package com.kafka.learn.kafkastudy.listener;
 
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 import org.springframework.kafka.annotation.KafkaListener;
 
@@ -7,7 +8,9 @@ import org.springframework.kafka.annotation.KafkaListener;
 public class RegularKafkaListener {
 
 	@KafkaListener(topics = "#{'${kafka.regular.topic}'}", containerFactory = "regularKafkaListenerContainerFactory")
-	public void consume(String message) {
-		System.out.println("Consumed message: " + message);
+	public void consume(Message<String> message) {
+
+		System.out.print("Consumed message: " + message.getPayload());
+		System.out.println(" Consumed header: " + message.getHeaders());
 	}
 }
