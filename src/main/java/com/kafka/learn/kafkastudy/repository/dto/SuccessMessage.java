3cd38284297e80419cc6c93491a2a98f7f3e02bc
SVAdithya <svadithya93@gmail.com>
2024-07-31 22:48:39 +0530

    using record instead of class
    using records instead of class, so we can avoid constructors, getter, setter, toString and hashMap

--- src/main/java/com/kafka/learn/kafkastudy/repository/dto/SuccessMessage.java
+++ src/main/java/com/kafka/learn/kafkastudy/repository/dto/SuccessMessage.java
@@ -1,20 +1,14 @@
 package com.kafka.learn.kafkastudy.repository.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "successMessage")
-@Getter
-@Setter
-@AllArgsConstructor
-public class SuccessMessage {
-	@Id
-	private String id;
-	private String headers;
-	private String body;
-	private Long offset;
-	private String time;
+public record SuccessMessage(
+		@Id String id,
+		String headers,
+		String body,
+		Long offset,
+		String time
+) {
 }
